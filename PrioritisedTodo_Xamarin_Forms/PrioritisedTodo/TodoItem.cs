5d43cc612a92020853ed0f9d22f513eeaf798514
Kym Phillpotts <kphillpotts@gmail.com>
2016-12-08 16:04:19 +1100

    Initial Commit

---
+++ PrioritisedTodo_Xamarin_Forms/PrioritisedTodo/TodoItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace PrioritisedTodo
+{
+	public class TodoItem
+	{
+		string id;
+		string name;
+		bool done;
+        string assignedTo;
+
+		[JsonProperty(PropertyName = "id")]
+		public string Id
+		{
+			get { return id; }
+			set { id = value;}
+		}
+
+		[JsonProperty(PropertyName = "text")]
+		public string Name
+		{
+			get { return name; }
+			set { name = value;}
+		}
+
+        [JsonProperty(PropertyName = "assignedto")]
+        public string AssignedTo
+        {
+            get { return assignedTo; }
+            set { assignedTo = value; }
+        }
+
+        [JsonProperty(PropertyName = "complete")]
+		public bool Done
+		{
+			get { return done; }
+			set { done = value;}
+		}
+
+        [Version]
+        public string Version { get; set; }
+	}
+}
+
---
+++ PrioritisedTodo_Xamarin_Forms/PrioritisedTodo/TodoItemManager.cs
@@ -0,0 +1,81 @@
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+
+namespace PrioritisedTodo
+{
+    public partial class TodoItemManager
+    {
+        static TodoItemManager defaultInstance = new TodoItemManager();
+        MobileServiceClient client;
+
+        IMobileServiceTable<TodoItem> todoTable;
+
+
+        private TodoItemManager()
+        {
+            this.client = new MobileServiceClient(Constants.ApplicationURL);
+
+            this.todoTable = client.GetTable<TodoItem>();
+        }
+
+        public static TodoItemManager DefaultManager
+        {
+            get
+            {
+                return defaultInstance;
+            }
+            private set
+            {
+                defaultInstance = value;
+            }
+        }
+
+        public MobileServiceClient CurrentClient
+        {
+            get { return client; }
+        }
+
+
+        public async Task<ObservableCollection<TodoItem>> GetTodoItemsAsync()
+        {
+            try
+            {
+                IEnumerable<TodoItem> items = await todoTable
+                    .Where(todoItem => !todoItem.Done)
+                    .ToEnumerableAsync();
+
+                return new ObservableCollection<TodoItem>(items);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"Sync error: {0}", e.Message);
+            }
+            return null;
+        }
+
+        public async Task SaveTaskAsync(TodoItem item)
+        {
+            if (item.Id == null)
+            {
+                await todoTable.InsertAsync(item);
+            }
+            else
+            {
+                await todoTable.UpdateAsync(item);
+            }
+        }
+
+    }
+}
